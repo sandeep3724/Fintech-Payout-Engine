7622394070344496835284717c324abf46613561
sandeep3724 <ankalapusandeep17@gmail.com>
2026-04-29 19:55:16 +0530

    Use DATABASE_URL for production database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,12 +97,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # =========================
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "playto_payout_db",
-        "USER": "postgres",
-        "PASSWORD": "3724",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
     }
 }
 
